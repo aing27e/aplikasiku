7dc8420069b49deebfd16a43e47ee4c33d4b9c65
aplikasiaceh <102360082+aplikasiaceh@users.noreply.github.com>
2022-04-01 00:20:32 +0700

    Add files via upload

---
+++ tes.php
@@ -0,0 +1,7 @@
+<?php
+/* Redirect browser */
+header("Location: http://begaltvku.site/ventidos/master-15.m3u8");
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
