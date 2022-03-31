4d654a031ef44846cf5c430e8dfeda7267dd245c
aplikasiaceh <102360082+aplikasiaceh@users.noreply.github.com>
2022-04-01 00:21:05 +0700

    Delete tes.php

--- tes.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Redirect browser */
-header("Location: http://begaltvku.site/ventidos/master-15.m3u8");
- 
-/* Make sure that code below does not get executed when we redirect. */
-exit;
-?>
